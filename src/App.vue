a59577c0125995296e2225ccc68b8663cba8bce9
manuel lopez <manuellh2008@gmail.com>
2020-12-25 22:23:32 -0600

    agregado select simple y select de objectos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <h1>
+        Ejemplo select vue
+      </h1>      
+    </header>
+    <main>
+      <section>
+        <article>
+          <h2>Seleccion</h2>
+          <select v-model="selected">
+            <option>Uno</option>
+            <option>Dos</option>
+            <option>Tres</option>
+          </select>
+        </article>
+        <article>
+          <h2>Seleccion de objectos</h2>
+          <select v-model="selectedObject">
+            <option v-bind:value="{nombre: 'uno', numero: 1}">Uno</option>
+            <option v-bind:value="{nombre: 'dos', numero: 2}">Dos</option>
+            <option v-bind:value="{nombre: 'tres', numero: 3}">Tres</option>
+          </select>
+        </article>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      selected: '',
+      selectedObject: {
+        nombre: '',
+        numero: 0
+      },
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
